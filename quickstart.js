ee1977a87542a6b415617c5ced3b47b5f9741db4
Michael Nahum <michaelnahum@Michaels-MBP.fios-router.home>
2018-05-28 22:31:49 -0400

    first commit

---
+++ quickstart.js
@@ -0,0 +1,158 @@
+// To reactivate:
+// 1) open in browser: http://localhost:8000/an_index.html
+// 2) cd into the goesBackGmail folder,
+// 3) type  python -m SimpleHTTPServer 8000 in the terminal.
+
+// Client ID and API key from the Developer Console
+
+
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete credentials.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const TOKEN_PATH = 'credentials.json';
+console.log(TOKEN_PATH)
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listLabels);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return callback(err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} An authorized OAuth2 client.
+ */
+function listLabels(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  gmail.users.labels.list({
+    userId: 'me',
+  }, (err, {data}) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const labels = data.labels;
+    if (labels.length) {
+      console.log('Labels:');
+      labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+      });
+    } else {
+      console.log('No labels found.');
+    }
+  });
+}
+
+function listMessages(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  gmail.users.messages.list({
+    userId: 'me',
+  }, (err, {data}) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const labels = data.labels;
+    if (labels.length) {
+      console.log('Labels:');
+      labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+      });
+    } else {
+      console.log('No labels found.');
+    }
+  });
+}
+// function getMessages(auth) {
+//   const gmail = google.gmail({version: 'v1', auth});
+//   gmail.users.messages.list({'userId': 'me'})
+//   .then(function(response) {
+//     console.log(response)
+//   }
+//  )
+// }
+
+//getMessages()
+
+/**   NO IDEA HOW THIS WORKS
+//https://developers.google.com/gmail/api/v1/reference/users/messages/list
+https://developers.google.com/gmail/api/v1/reference/users/messages/get
+ * Retrieve Messages in user's mailbox matching query.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} query String used to filter the Messages listed.
+ * @param  {Function} callback Function to call when the request is complete.
+
+function listMessages(me, query, callback) {
+  var getPageOfMessages = function(request, result) {
+    request.execute(function(resp) {
+      result = result.concat(resp.messages);
+      var nextPageToken = resp.nextPageToken;
+      if (nextPageToken) {
+        request = gapi.client.gmail.users.messages.list({
+          'userId': me,
+          'pageToken': nextPageToken,
+          'q': query
+        });
+        getPageOfMessages(request, result);
+      } else {
+        callback(result);
+      }
+    });
+  };
+  var initialRequest = gapi.client.gmail.users.messages.list({
+    'userId': me,
+    'q': query
+  });
+  getPageOfMessages(initialRequest, []);
+}
+*/
